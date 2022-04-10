bc8b1e8a780aa6399098d667832782e60468257c
Ana Gaitan <anais6622@hotmail.com>
2022-04-10 00:00:04 -0500

    Se agrega indicador de cantidad de productos en el carro, que se actualiza en tiempo real

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Badge } from "react-bootstrap";
 import { BsFillCartFill } from "react-icons/bs";
+import { useCartContext } from "../../context/CartContext";
 
 const CartWidget = () => {
+  const { cartContent } = useCartContext();
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(cartContent.cantidadTotal);
+  }, [cartContent]);
+
   return (
     <div>
-      <BsFillCartFill />
+      {cartContent.list.length <= 0 ? (
+        <></>
+      ) : (
+        <>
+          <BsFillCartFill />
+          <Badge pill bg="success">
+            {total}
+          </Badge>{" "}
+        </>
+      )}
     </div>
   );
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,22 +1,44 @@
 import { createContext, useContext, useState } from "react";
 
-const CartContext = createContext([]);
+const CartContext = createContext({});
 
 export const useCartContext = () => useContext(CartContext);
 
 export default function CartContextProvider({ children }) {
-  const [cartList, setCartList] = useState([]);
+  const [cartContent, setCartContent] = useState({
+    cantidadTotal: 0,
+    list: [],
+  });
 
   const addToCart = (item) => {
     if (isInCart(item)) {
       getFoundItem(item).cantidad += item.cantidad;
+      setCartContent({
+        cantidadTotal: cartContent.cantidadTotal + item.cantidad,
+        list: [...cartContent.list],
+      });
     } else {
-      setCartList([...cartList, item]);
+      setCartContent({
+        cantidadTotal: cartContent.cantidadTotal + item.cantidad,
+        list: [...cartContent.list, item],
+      });
     }
   };
 
-  const removeCart = () => {
-    setCartList([]);
+  const removeFromCart = (item) => {
+    const index = cartContent.list.findIndex((e) => e.item.id === item.item.id);
+    cartContent.list.splice(index, 1);
+    setCartContent({
+      cantidadTotal: cartContent.cantidadTotal - item.cantidad,
+      list: [...cartContent.list],
+    });
+  };
+
+  const cleanCart = () => {
+    setCartContent({
+      cantidadTotal: 0,
+      list: [],
+    });
   };
 
   const isInCart = (item) => {
@@ -25,14 +47,15 @@ export default function CartContextProvider({ children }) {
   };
 
   const getFoundItem = (item) =>
-    cartList.find((i) => i.item.id === item.item.id);
+    cartContent.list.find((i) => i.item.id === item.item.id);
 
   return (
     <CartContext.Provider
       value={{
-        cartList,
+        cartContent,
         addToCart,
-        removeCart,
+        removeFromCart,
+        cleanCart,
       }}
     >
       {children}
